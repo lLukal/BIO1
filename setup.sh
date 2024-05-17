0ad42ec8f9980bb598792035b990977fb165f399
Luka Milicevic <lukalmx@gmail.com>
2024-05-17 22:23:14 +0200

    improve setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 curl https://nanopore.s3.climb.ac.uk/MAP006-1_2D_pass.fasta > ./input/MAP006-1_2D_pass.fasta
+
+pip install biopython
+
+# Or
+# conda install biopython
+
+# To get started, use:
+# bash run.sh --reference input/GCF_000005845.2_ASM584v2_genomic.fna --fragments input/MAP006-1_2D_pass.fasta --debug
